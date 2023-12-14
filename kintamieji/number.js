a0d0816570de4d5fafe720a8e1b1f9e3d22dbab2
Rimantas <belauzas@gmail.com>
2023-12-14 11:33:15 +0200

    Number

---
+++ kintamieji/number.js
@@ -0,0 +1,51 @@
+/*
+NUMBERS
+
+- sveikieji / desimtainiai
+    sveikieji: 1, 2, 5, -7
+    desimtainiai: 3.14, -2.727
+- teigiami / neigiami
+    teigiami: 1, 2.5
+    neigiami: -1, -2.5
+- normalus / "ne normalus"
+    normalus: visi virsuje
+    "ne normalus": NaN (not-a-number), Infinity, -Infinity
+
+KINTAMUJU INICIAVIMAS:
+- const (default)
+- let (kai reiksme tures keistis)
+- var (NIEKADA, nebent zinai ka darai ir megsti nuotykius 👀)
+
+*/
+
+const a = 5;
+console.log('a');
+console.log(a);
+
+const b = 3.14;
+console.log(b);
+
+const c = -77;
+console.log(c);
+
+const d = -2.727;
+console.log(d);
+
+console.log(0);
+console.log(-0);
+
+const e = Infinity;
+const f = -Infinity;
+console.log(e, f);
+
+console.log('labas', 123, a, b, 'rytas');
+
+const gg = 777;
+console.log(gg);
+
+
+const n1 = 8;
+const n2 = 6;
+const suma = n1 + n2;
+
+console.log(n1, '+', n2, '=', suma);
