eed1f1df457f62434bebbe7e4bfc19f8bde3a59d
Rimantas <belauzas@gmail.com>
2023-12-20 11:24:47 +0200

    Function

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ Multi
 line
 comment
 */
+
+/*
+BAZINIAI PROGRAMAVIMO KONCEPTAI:
+- duomenu tipai:
+    - number
+    - string
+    - boolean
+    - array
+    - undefined
+- if
+- for
+- function
+- events
+*/
