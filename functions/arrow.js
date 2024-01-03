573adb9b93bbbce14f3b56b182c7aa8d6afba483
Rimantas <belauzas@gmail.com>
2024-01-03 11:20:59 +0200

    Abbriavation function

--- functions/arrow.js
+++ functions/arrow.js
@@ -34,3 +34,34 @@ console.log(`${n1} * ${n1} = ${square(n1)}`);
 const cube = a => a ** 3;
 console.log(`${n2} * ${n2} * ${n2} = ${cube(n2)}`);
 
+
+// Chuck Norris -> C.N.
+// Pamela Anderson -> P.A.
+
+function abbriavation(text) {
+    const words = text.split(' ');
+    let ans = '';
+
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        ans += word[0] + '.';
+    }
+
+    return ans;
+}
+
+const abbr = t => t.split(' ').map(s => s[0] + '.').join('');
+
+console.log(abbriavation('Jeses'));
+console.log(abbriavation('Chuck Norris'));
+console.log(abbriavation('Pamela Anderson'));
+console.log(abbriavation('Jan Clod VanDam'));
+console.log(abbriavation('Jan Clod Van Dam'));
+console.log(abbriavation('Super Jan Clod Van Dam'));
+console.log('---------------------------------------');
+console.log(abbr('Jeses'));
+console.log(abbr('Chuck Norris'));
+console.log(abbr('Pamela Anderson'));
+console.log(abbr('Jan Clod VanDam'));
+console.log(abbr('Jan Clod Van Dam'));
+console.log(abbr('Super Jan Clod Van Dam'));
